7e3e85e863f78cd2a6b960798e15ba7360ed1c80
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-11-14 18:09:52 +0200

    Temporary solution for V1 graph.

--- src/components/V1.js
+++ src/components/V1.js
@@ -23,13 +23,35 @@ ChartJS.register(
 );
 
 export default function V1() {
-  const [result, setResult] = useState([]);
+  const [result1, setResult1] = useState([]);
+  const [result2, setResult2] = useState([]);
+  const [result3, setResult3] = useState([]);
+  const [result4, setResult4] = useState([]);
+  const [result5, setResult5] = useState([]);
+  const [result6, setResult6] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const asyncFunction = async () => {
-      const response = await fetch("http://localhost:3001/1");
-      const json = await response.json();
-      setResult(json);
+      const response1 = await fetch("http://localhost:3001/1");
+      const json1 = await response1.json();
+      setResult1(json1);
+      const response2 = await fetch("http://localhost:3001/2");
+      const json2 = await response2.json();
+      setResult2(json2);
+      const response3 = await fetch("http://localhost:3001/3");
+      const json3 = await response3.json();
+      setResult3(json3);
+      const response4 = await fetch("http://localhost:3001/4");
+      const json4 = await response4.json();
+      setResult4(json4);
+      const response5 = await fetch("http://localhost:3001/5");
+      const json5 = await response5.json();
+      setResult5(json5);
+      const response6 = await fetch("http://localhost:3001/6");
+      const json6 = await response6.json();
+      setResult6(json6);
+      setIsLoading(false);
     };
     asyncFunction();
   }, []);
@@ -47,19 +69,94 @@ export default function V1() {
     },
   };
 
-  const labels = result.map((item) => item.measurement_date);
+  const dataSet = (int) => {
+    switch (int) {
+      case 4:
+        let returnArray4 = [];
+        let pos4 = 0;
+        for (let i = 0; i < result4.length; i++) {
+          const object = {
+            x: result1[pos4].measurement_date,
+            y: result4[i].data,
+          };
+          returnArray4.push(object);
+          pos4 += 12;
+        }
+        return returnArray4;
+      case 5:
+        let returnArray5 = [];
+        let pos5 = 0;
+        for (let i = 0; i < result5.length; i++) {
+          const object = {
+            x: result1[pos5].measurement_date,
+            y: result5[i].data,
+          };
+          returnArray5.push(object);
+          pos5 += 12;
+        }
+        return returnArray5;
+      case 6:
+        let returnArray6 = [];
+        let pos6 = 0;
+        for (let i = 0; i < result6.length; i++) {
+          const object = {
+            x: result1[pos6].measurement_date,
+            y: result6[i].data,
+          };
+          returnArray6.push(object);
+          pos6 += 12;
+        }
+        return returnArray6;
+      default:
+        return "Something went wrong when assigning data";
+    }
+  };
 
   const data = {
-    labels,
+    labels: result1.map((item) => item.measurement_date),
     datasets: [
       {
         label: "Dataset 1",
-        data: result.map((item) => item.data),
+        data: result1.map((item) => item.data),
         borderColor: "rgb(255, 99, 132)",
         backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: result2.map((item) => item.data),
+        borderColor: "rgb(45, 199, 232)",
+        backgroundColor: "rgba(45, 199, 132, 0.5)",
+      },
+      {
+        label: "Dataset 3",
+        data: result3.map((item) => item.data),
+        borderColor: "rgb(2, 233, 32)",
+        backgroundColor: "rgba(2, 233, 32, 0.5)",
+      },
+      {
+        label: "Dataset 4",
+        data: dataSet(4),
+        borderColor: "rgb(15, 15, 32)",
+        backgroundColor: "rgba(15, 15, 32, 0.5)",
+      },
+      {
+        label: "Dataset 4",
+        data: dataSet(5),
+        borderColor: "rgb(15, 213, 32)",
+        backgroundColor: "rgba(15, 213, 32, 0.5)",
+      },
+      {
+        label: "Dataset 4",
+        data: dataSet(6),
+        borderColor: "rgb(88, 15, 32)",
+        backgroundColor: "rgba(88, 15, 32, 0.5)",
       },
     ],
   };
 
-  return <Line options={options} data={data} />;
+  if (isLoading) {
+    return <p>Loading... </p>;
+  } else {
+    return <Line options={options} data={data} />;
+  }
 }
