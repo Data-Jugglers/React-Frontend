6b1194b338f9b42c5725e0ba187fc9c863049196
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-11-17 20:38:20 +0200

    Implemented v2

--- src/components/V1.js
+++ src/components/V1.js
@@ -5,14 +5,18 @@ import { useEffect, useState } from "react";
 import "chartjs-adapter-luxon";
 
 export default function V1() {
-  const [result, setResult] = useState([]);
+  const [v1, setV1] = useState([]);
+  const [v2, setV2] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const asyncFunction = async () => {
       const response = await fetch("http://localhost:3001/v1");
       const json = await response.json();
-      setResult(json);
+      setV1(json);
+      const response2 = await fetch("http://localhost:3001/v2");
+      const json2 = await response2.json();
+      setV2(json2);
       setIsLoading(false);
     };
     asyncFunction();
@@ -27,6 +31,9 @@ export default function V1() {
       title: {
         display: true,
         text: "Historic temperature anomalies",
+        font: {
+          size: 18,
+        },
       },
     },
     borderWidth: 1,
@@ -51,59 +58,83 @@ export default function V1() {
           data={{
             datasets: [
               {
-                label: "Global monthly",
-                data: result[0].map((item) => ({
+                label: "Global Monthly",
+                data: v1[0].map((item) => ({
+                  x: item.measurement_date,
+                  y: item.data,
+                })),
+                borderColor: "rgb(255, 99, 132, 0.8)",
+              },
+              {
+                label: "Global Annual",
+                data: v1[3].map((item) => ({
                   x: item.measurement_date,
                   y: item.data,
                 })),
                 borderColor: "rgb(255, 99, 132)",
               },
               {
-                label: "Northern monthly",
-                data: result[1].map((item) => ({
+                label: "Northern Monthly",
+                data: v1[1].map((item) => ({
+                  x: item.measurement_date,
+                  y: item.data,
+                })),
+                borderColor: "rgb(45, 199, 232, 0.8)",
+              },
+              {
+                label: "Northern Annual",
+                data: v1[4].map((item) => ({
                   x: item.measurement_date,
                   y: item.data,
                 })),
                 borderColor: "rgb(45, 199, 232)",
               },
               {
-                label: "Southern monthly",
-                data: result[2].map((item) => ({
+                label: "Southern Monthly",
+                data: v1[2].map((item) => ({
+                  x: item.measurement_date,
+                  y: item.data,
+                })),
+                borderColor: "rgb(2, 233, 32, 0.8)",
+              },
+              {
+                label: "Southern Annual",
+                data: v1[5].map((item) => ({
                   x: item.measurement_date,
                   y: item.data,
                 })),
                 borderColor: "rgb(2, 233, 32)",
               },
               {
-                label: "Global yearly",
-                data: result[3].map((item) => ({
+                label: "Past 2000 years",
+                data: v2[0].map((item) => ({
                   x: item.measurement_date,
                   y: item.data,
                 })),
-                borderColor: "rgb(15, 15, 32)",
-              },
-              {
-                label: "Northern yearly",
-                data: result[4].map((item) => ({
-                  x: item.measurement_date,
-                  y: item.data,
-                })),
-                borderColor: "rgb(155, 130, 213)",
-              },
-              {
-                label: "Southern yearly",
-                data: result[5].map((item) => ({
-                  x: item.measurement_date,
-                  y: item.data,
-                })),
-                borderColor: "rgb(88, 15, 32)",
+                borderColor: "rgb(50,30,70)",
               },
             ],
           }}
         />
-        <a href={result[6][0].source_link}>Source Link</a>
-        <a href={result[6][0].description_link}>Description Link</a>
-        <p>{result[6][0].description}</p>
+        <div className="graphDescription">
+          <p className="description">{v1[6][0].description}</p>
+          <p className="description">{v2[1][0].description}</p>
+        </div>
+        <div className="graphLinks">
+          <p className="link">
+            Link to HadCRUT5 <a href={v1[6][0].source_link}>source data.</a>
+            <br />
+            Link to official HadCRUT5 data description:{" "}
+            <a href={v1[6][0].description_link}>description</a>
+          </p>
+          <p className="link">
+            Link to 2,000-year temperature reconstruction{" "}
+            <a href={v2[1][0].source_link}>source data</a>
+            <br />
+            Link to 2,000-year temperature reconstruction description:{" "}
+            <a href={v2[1][0].description_link}>description</a>
+          </p>
+        </div>
       </div>
     );
   }
