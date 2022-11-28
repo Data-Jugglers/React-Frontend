9b779b10be31c783decc30fdc2923e2f8592fd54
unknown <89990168+WellyBelly@users.noreply.github.com>
2022-11-28 19:46:17 +0200

    Implemented V8 to views

--- src/pages/N1.js
+++ src/pages/N1.js
@@ -1,12 +1,8 @@
 import React from "react";
 
-
-
 import V4 from "../components/V4";
 
-
 import { V1, V3, V5, V7 } from "../components";
-
 
 import "../styles/graphComponent.css";
 
@@ -20,7 +16,6 @@ export default function N1() {
         <V4 />
         <V5 />
         <V7 />
-
       </div>
     </>
   );
