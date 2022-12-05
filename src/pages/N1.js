5aa37f255a84dc19aad9211dd06d964826a9b037
moeez <moeezk115@gmail.com>
2022-12-05 13:11:45 +0200

    V9 partially done

--- src/pages/N1.js
+++ src/pages/N1.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-
-
 import V4 from "../components/V4";
-
+import V9 from "../components/V9";
 
 import { V1, V3, V5, V7 } from "../components";
-
 
 import "../styles/graphComponent.css";
 
@@ -20,7 +17,7 @@ export default function N1() {
         <V4 />
         <V5 />
         <V7 />
-
+        <V9 />
       </div>
     </>
   );
