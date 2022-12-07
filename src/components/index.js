0feea7afcb52220fea376d9108b42f91de057400
Ivan Trufanov <90568444+pochapali@users.noreply.github.com>
2022-12-07 09:22:23 +0000

    added V6 to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import V1 from "./V1";
 import V3 from "./V3";
 import V4 from "./V4";
 import V5 from "./V5";
+import V6 from "./V6";
 import V7 from "./V7";
 import V8 from "./V8";
 import V9 from "./V9";
